00405b6553fbf0b83cf74de0998ce4135d00baef
matelat-daw <cesarmatelat@gmail.com>
2026-03-10 18:40:06 +0000

    Update - Energy - Mailer

---
+++ Project-Energy/api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'energy/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e0b345e146c2daf6d04c13c1f8c32307c7dcd57b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'energy/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e0b345e146c2daf6d04c13c1f8c32307c7dcd57b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
